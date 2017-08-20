5f63351e13e334e1dd72ffec90e90347796ee05f
Kingkun233 <930314850@qq.com>
2017-08-20 23:21:16 +0800

    0.1

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 $config = [
     'dbconfig' => [
         'dbuser' => 'root',
-        'dbpsw' => '',
+        'dbpsw' => '4411918471',
         'dbcharset' => 'utf8',
         'dsn' => "mysql:host=localhost;dbname=kinPHP"
     ]
